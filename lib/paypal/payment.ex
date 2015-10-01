46ee0c6c64bc002a42265027ae42eed6740260c1
Elias Granja <contato@eliasgranja.com>
2015-09-30 21:20:55 -0300

    Add update_payment and get_payments in the Paypal wrapper

--- lib/paypal/payment.ex
+++ lib/paypal/payment.ex
@@ -1,9 +1,15 @@
 defmodule Paypal.Payment do
   @moduledoc """
   This module is responsible for handle all payment calls to paypal.
+  The sctruct has the following keys:
+  intent  string  Payment intent. Must be set to sale for immediate payment, authorize to authorize a payment for capture later, or order to create an order. Required.
+  payer payer Source of the funds for this payment represented by a PayPal account or a credit card. Required.
+  transactions  array of transaction objects  Transactional details including the amount and item details. Required.
+  redirect_urls redirect_urls Set of redirect URLs you provide only for PayPal-based payments. Required for PayPal payments.
+  update: used at the Payment.update protocol.
   """
   @derive [Poison.Encoder]
-  defstruct intent: nil, payer: nil, transactions: nil, id: nil
+  defstruct intent: nil, payer: nil, transactions: nil, id: nil, op: nil, update: nil
 
 end
 
@@ -14,6 +20,8 @@ defimpl Payment, for: Paypal.Payment do
     the Paypal API.
 
     Example of payment struct: %Paypal.Payment{"intent" => "sale", "payer" => %{"funding_instruments" => [%{"credit_card" => %{"billing_address" => %{"city" => "Saratoga", "country_code" => "US", "line1" => "111 First Street", "postal_code" => "95070", "state" => "CA"}, "cvv2" => "874", "expire_month" => 11, "expire_year" => 2018, "first_name" => "Betsy", "last_name" => "Buyer", "number" => "4417119669820331", "type" => "visa"}}], "payment_method" => "credit_card"}, "transactions" => [%{"amount" => %{"currency" => "USD", "details" => %{"shipping" => "0.03", "subtotal" => "7.41", "tax" => "0.03"}, "total" => "7.47"}, "description" => "This is the payment transaction description."}]}
+    The information about the value of the keys are in https://developer.paypal.com/webapps/developer/docs/api/#create-a-payment
+    
     Returns a Task.
   """
   def create_payment(payment) do
@@ -21,16 +29,42 @@ defimpl Payment, for: Paypal.Payment do
   end
 
   defp do_create_payment(payment) do 
-    HTTPoison.post(Paypal.Config.url <> "/payments/payment", Poison.encode!(payment),  Paypal.Authentication.headers, timeout: :infinity, recv_timeout: :infinity)
+    string_payment = format_create_payment_request  Poison.encode!(payment)
+    HTTPoison.post(Paypal.Config.url <> "/payments/payment", string_payment,  Paypal.Authentication.headers, timeout: :infinity, recv_timeout: :infinity)
     |> Paypal.Config.parse_response
   end
+
+  defp format_create_payment_request(payment) do
+    String.replace(payment, ~s("op":null,), "")
+    |> String.replace(~s("update":null,), "")
+  end
+
   @doc """
   Function to get the status of the payment at Paypal. It returns the API JSON as a dict.
   It receives a Paypal.Payment struct with id.
-
   """
   def get_status(payment) do
     HTTPoison.get(Paypal.Config.url <> "/payments/payment/" <> payment.id, Paypal.Authentication.headers, timeout: :infinity, recv_timeout: :infinity)
+    |> Paypal.Config.parse_response
+  end
+
+  @doc """
+  Function to update payment fields. You have to pass in the update key a list with a HashDict with following keys: 
+  op  string  Patch operation to perform. Allowed values: add and replace.
+  path  string  String containing a JSON-Pointer value that references a location within the target document (the target location) where the operation is performed.
+  value object  New value to apply based on the operation. Allowed objects are amount object or shipping_address object.
+  For more details see the Paypal Documentation: https://developer.paypal.com/webapps/developer/docs/api/#update-a-payment-resource
+  """
+  def update_payment(payment) do
+      HTTPoison.patch(Paypal.Config.url <>  "/payments/payment/" <> payment.id, Poison.encode!(payment.update), Paypal.Authentication.headers, timeout: :infinity, recv_timeout: :infinity)
+      |> Paypal.Config.parse_response
+  end
+  @doc """
+  Use this call to get a list of payments in any state (created, approved, failed, etc.). The payments returned are the payments made to the merchant making the call.
+  You should pass to this function just a empty Paypal.Payment like %Paypal.Payment{}
+  """
+  def get_payments(_payment) do
+    HTTPoison.get(Paypal.Config.url <> "/payments/payment/", Paypal.Authentication.headers, timeout: :infinity, recv_timeout: :infinity)
     |> Paypal.Config.parse_response
   end
 end
--- test/paypal/payment_test.exs
+++ test/paypal/payment_test.exs
@@ -4,11 +4,27 @@ defmodule Paypal.PaymentTest do
   @tag timeout: :infinity
   test "create and get payment" do
     Paypal.App.start([],[])
-    
+
   	{:ok, result} = Task.await(Payment.create_payment(%Paypal.Payment{intent: "sale", payer: %{"funding_instruments" => [%{"credit_card" => %{"billing_address" => %{"city" => "Saratoga", "country_code" => "US", "line1" => "111 First Street", "postal_code" => "95070", "state" => "CA"}, "cvv2" => "874", "expire_month" => 11, "expire_year" => 2018, "first_name" => "Betsy", "last_name" => "Buyer", "number" => "4417119669820331", "type" => "visa"}}], "payment_method" => "credit_card"}, transactions: [%{"amount" => %{"currency" => "USD", "details" => %{"shipping" => "0.03", "subtotal" => "7.41", "tax" => "0.03"}, "total" => "7.47"}, "description" => "This is the payment transaction description."}]}), 10000000)
   	assert "approved" == result["state"]
 
     {:ok, status} = Payment.get_status %Paypal.Payment{id: result["id"]}
     assert "approved" == status["state"]
   end
+
+  test "get a list of payments" do
+    Paypal.App.start([],[])
+    assert {:ok, _} = Payment.get_payments(%Paypal.Payment{})
+
+  end
+
+#  @tag timeout: :infinity
+ # test "Update a payment" do 
+ #   Paypal.App.start([],[])
+
+   # {:ok, result} = Task.await(Payment.create_payment(%Paypal.Payment{intent: "authorize", payer: %{"funding_instruments" => [%{"credit_card" => %{"billing_address" => %{"city" => "Saratoga", "country_code" => "US", "line1" => "111 First Street", "postal_code" => "95070", "state" => "CA"}, "cvv2" => "874", "expire_month" => 11, "expire_year" => 2018, "first_name" => "Betsy", "last_name" => "Buyer", "number" => "4417119669820331", "type" => "visa"}}], "payment_method" => "credit_card"}, transactions: [%{"amount" => %{"currency" => "USD", "details" => %{"shipping" => "0.03", "subtotal" => "7.41", "tax" => "0.03"}, "total" => "7.47"}, "description" => "This is the payment transaction description."}]}), 10000000)
+   ## {:ok, payment} = Payment.update_payment %Paypal.Payment{id: result["id"], update: []} # update: [%{"op" => "replace", "path" => "/transactions/0/amount", "value" => %{"currency" => "EUR", "details" => %{"shipping" => "5.00", "subtotal" => "13.37"}, "total" => "18.37"}}]
+    #IO.puts inspect payment
+    #assert result["id"] == payment["id"]
+  #end
 end
