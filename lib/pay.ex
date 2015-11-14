7d48b1227764917799d975a775f3565db8c5e14f
Elias Granja <contato@eliasgranja.com>
2015-11-14 11:16:38 +0000

    Add start_link to Pay

--- lib/pay.ex
+++ lib/pay.ex
@@ -1,6 +1,7 @@
 defmodule Pay do
   use Application
 
+  def start_link(type, args), do: start(type, args)
   def start(type,args) do
     case Application.get_env(:pay, :type) do
       :paypal -> Paypal.App.start(type, args)
