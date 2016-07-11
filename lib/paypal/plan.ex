78f2515fdbd1468b679f1d896bd7d0bb280ecacc
stephen <huangjun604@gmail.com>
2016-07-12 09:13:20 +1000

    Add billing plan and agreement feature

---
+++ lib/paypal/plan.ex
@@ -0,0 +1,42 @@
+defmodule Paypal.Plan do
+  @derive [Poison.Encoder]
+  defstruct id: nil, name: nil, description: nil, type: nil, payment_definitions: nil, merchant_preferences: nil
+end
+
+defimpl Plan, for: Paypal.Plan do
+
+  def create(plan) do
+    Task.async(fn -> do_create(plan) end)
+  end
+
+  defp do_create(plan) do
+    string_plan = Poison.encode!(plan)
+    HTTPoison.post(Paypal.Config.url <> "/payments/billing-plans", string_plan,
+      Paypal.Authentication.headers, timeout: :infinity, recv_timeout: :infinity)
+    |> parse_response()
+  end
+
+  def update(plan) do
+    Task.async fn -> do_update(plan) end
+  end
+
+  defp do_update(plan) do
+    IO.inspect Paypal.Authentication.headers
+    HTTPoison.patch(Paypal.Config.url <> "/payments/billing-plans/#{plan.id}",
+      Poison.encode!([%{path: "/", value: %{"state" => "ACTIVE"}, op: "replace"}]),
+      Paypal.Authentication.headers, timeout: :infinity, recv_timeout: :infinity)
+  end
+
+  defp parse_response(response) do
+    case response do
+      {:ok, %HTTPoison.Response{status_code: 401,  body: body, headers: _headers}} ->
+        {:ok, response} = Poison.decode body
+        {:auth_error, response}
+      {:ok, %HTTPoison.Response{status_code: _, body: body, headers: _headers}} ->
+        {:ok, Poison.decode! body}
+      {:error, %HTTPoison.Error{reason: reason}} ->
+        {:nok, reason}
+    end
+  end
+
+end
